55b07078841f948f160835d7a946684204251ec9
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-06-04 14:54:10 -0400

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, 'src/'),
+      "~": path.resolve(__dirname, '')
+    }
+  }
+}
