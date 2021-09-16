e4ef0c373771570d4dc7a87b7ff5147580a922bf
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-09-16 13:17:35 -0400

    Restructure code and display in console

--- src/index.js
+++ src/index.js
@@ -1,69 +1,51 @@
-import { lab } from 'd3';
-import _ from 'lodash';
-import * as getData from '@/getRepoData.js'
-//Get manually created list of issues, labels and milestones
-import issues from "~/repoIssues.json"
-import labels from "~/repoLabels.json"
-import milestones from "~/repoMilestones.json"
+// Get manually created list of issues, labels and milestones
+// import issues from '~/repoIssues.json';
+// import labels from '~/repoLabels.json';
+// import milestones from '~/repoMilestones.json';
+import { getIssuesList, getMilestonesList } from './getRepoData';
 
+// Set org, repo and number per page
+const config = {
+  org: 'sara-sabr',
+  repo: 'ITStrategy',
+  per_page: 100,
+};
 
-const myIssues = issues
+const issues = getIssuesList(config.org, config.repo, config.per_page);
+const milestones = getMilestonesList(config.org, config.repo, config.per_page);
+// eslint-disable-next-line no-console
+console.log(issues);
+// eslint-disable-next-line no-console
+console.log(milestones);
 
-function isInitiative(labelName, prefix) {
-  let isValid = false;
-  if (typeof labelName === "string" || labelName !== '') {
-    isValid = true;
-  } else {
-    return 0
-  }
-
-  if (isValid === true) {
-    let prefixIndex = labelName.indexOf(prefix)
-  } else {
-    return 0
-  }
-
-}
-
-function findInitiativesIDsFromLabels() {
-  let initiativesIDs = [];
-  if (Array.isArray(labels)) {
-    console.log("this is an array")
-    labels.forEach(label => {
-      if (label.name.includes("initiative:")) {
-        console.log("name: " + label.name)
-        if (isInitiative(label.name, "initiative:") !== 0){
-          initiativesIDs.push(label.id)
-        }
-      }
-    })
-  }
-  console.log(initiativesIDs)
-}
-// function component() {
-
-//   let counter = 0;
-//   for ( element in issues){
-//     const element = document.createElement('div');
-//     element.innerHTML = _.join(element);
+// function isInitiative(labelName, prefix) {
+//   let isValid = false;
+//   if (typeof labelName === 'string' || labelName !== '') {
+//     isValid = true;
+//   } else {
+//     return 0;
 //   }
 
-//   const element = document.createElement('div');
-
-//   // Lodash, currently included via a script, is required for this line to work
-//   // Lodash, now imported by this script
-//   element.innerHTML = _.join(['Magic', 'waiting', "to", "happen"], ' ');
-
-//   return element;
+//   if (isValid === true) {
+//     const prefixIndex = labelName.indexOf(prefix);
+//   } else {
+//     return 0;
+//   }
+//   return isValid;
 // }
-let ghIssues = getData.listIssues("sara-sabr", "ITStrategy", 100)
 
-console.log(ghIssues)
-let thisIssue = issues[0]
-let thisLabel = labels[0]
-let thisMilestone = milestones[0]
-// console.log(thisIssue)
-// console.log(thisLabel)
-// console.log(thisMilestone)
-findInitiativesIDsFromLabels()
-// document.body.appendChild(component());
+// function findInitiativesIDsFromLabels() {
+//   const initiativesIDs = [];
+//   if (Array.isArray(labels)) {
+//     console.log('this is an array');
+//     labels.forEach((label) => {
+//       if (label.name.includes('initiative:')) {
+//         console.log(`name: ${label.name}`);
+//         if (isInitiative(label.name, 'initiative:') !== 0) {
+//           initiativesIDs.push(label.id);
+//         }
+//       }
+//     });
+//   }
+//   console.log(initiativesIDs);
+// }
