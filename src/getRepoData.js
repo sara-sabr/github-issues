431fb6e5bbc77fe645e7cb9e0f79e3faa553afca
Guillaume Charest <guillaume.w.charest@gmail.com>
2021-09-16 13:14:43 -0400

    Cleanup getRepoData functions

--- src/getRepoData.js
+++ src/getRepoData.js
@@ -1,51 +1,79 @@
-import _ from 'lodash';
 import { Octokit } from 'octokit';
 
-const octokit = new Octokit
-
-export async function listIssues(owner, repo, per_page) {
+const octokit = new Octokit();
+// eslint-disable-next-line camelcase
+async function listIssues(owner, repo, per_page) {
   const issues = await octokit.paginate(octokit.rest.issues.listForRepo, {
-    owner: owner,
-    repo: repo,
-    per_page: per_page,
+    owner,
+    repo,
+    per_page,
+  }).then((value) => {
+    if (!value) {
+      throw Error('Error loading issues');
+    }
+    return value;
   });
-  issues.forEach(issue => {
-    const element = document.createElement('div');
-    element.innerHTML = JSON.stringify(issue);
-    // element.innerHTML = issue;
-    document.body.appendChild(element)
-  })
-  console.log(JSON.stringify(issues[0]))
+  return issues;
+}
+// eslint-disable-next-line camelcase
+async function listRepoMilestones(owner, repo, per_page) {
+  const repoMilestones = await octokit.paginate(octokit.rest.issues.listMilestones, {
+    owner,
+    repo,
+    per_page,
+  });
+  return repoMilestones;
+}
+// eslint-disable-next-line camelcase
+async function listRepoLabels(owner, repo, per_page) {
+  const repoLabels = await octokit.paginate(octokit.rest.issues.listLabelsForRepo, {
+    owner,
+    repo,
+    per_page,
+  });
+  return repoLabels;
 }
 
-export async function listRepoMilestones(owner, repo, per_page) {
-  const issues = await octokit.paginate(octokit.rest.issues.listMilestones, {
-    owner: owner,
-    repo: repo,
-    per_page: per_page,
-  });
-  issues.forEach(issue => {
-    const element = document.createElement('div');
-    element.innerHTML = JSON.stringify(issue);
-    // element.innerHTML = issue;
-    document.body.appendChild(element)
-  })
-  console.log(JSON.stringify(issues[0]))
+// eslint-disable-next-line camelcase
+export function getIssuesList(owner, repo, per_page) {
+  const issuesList = [];
+  listIssues(owner, repo, per_page).then((response) => {
+    response.forEach((value) => {
+      issuesList.push(value);
+    });
+  }).catch((err) => { throw Error(err); });
+  if (issuesList) {
+    return issuesList;
+  }
+  return undefined;
 }
 
-export async function listRepoLabels(owner, repo, per_page) {
-  const issues = await octokit.paginate(octokit.rest.issues.listLabelsForRepo, {
-    owner: owner,
-    repo: repo,
-    per_page: per_page,
-  });
-  issues.forEach(issue => {
-    const element = document.createElement('div');
-    element.innerHTML = JSON.stringify(issue);
-    // element.innerHTML = issue;
-    document.body.appendChild(element)
-  })
-  console.log(JSON.stringify(issues[0]))
+// eslint-disable-next-line camelcase
+export function getMilestonesList(owner, repo, per_page) {
+  const milestonesList = [];
+  listRepoMilestones(owner, repo, per_page).then((response) => {
+    response.forEach((value) => {
+      milestonesList.push(value);
+    });
+  }).catch((err) => { throw Error(err); });
+  if (milestonesList) {
+    return milestonesList;
+  }
+  return undefined;
+}
+
+// eslint-disable-next-line camelcase
+export function getRepoLabelList(owner, repo, per_page) {
+  const repoLabelsList = [];
+  listRepoLabels(owner, repo, per_page).then((response) => {
+    response.forEach((value) => {
+      repoLabelsList.push(value);
+    });
+  }).catch((err) => { throw Error(err); });
+  if (repoLabelsList) {
+    return repoLabelsList;
+  }
+  return undefined;
 }
 
 // listRepoLabels("sara-sabr", "ITStrategy", 100);
